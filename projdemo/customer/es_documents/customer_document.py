51ae43b12c3930066e91450cd6611ddd2e5053ba
ashish <sondagarashish@gmail.com>
2020-03-11 18:19:31 +0530

    customer-es-document

--- projdemo/customer/es_documents/customer_document.py
+++ projdemo/customer/es_documents/customer_document.py
@@ -4,3 +4,13 @@ from customer.models.customer import (Customer)
 
 customer_index = Index('customers')
 customer_index.settings(number_of_shards=1,number_of_replicas=0)
+
+
+@customer_index.doc_type
+class CustomerDocument(DocType):
+    name = fields.TextField(
+        attr='name',
+        fields={
+            'suggest': fields.Completion(),
+        }
+    )
